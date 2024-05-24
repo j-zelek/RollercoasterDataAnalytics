b571cea8d3a3f81e7c5a59db56240d1e4e33f74e
j-zelek <jerome@zelek.biz>
2024-05-24 22:48:58 +0200

    add function to get waiting times

---
+++ RollercoasterDataAnalytics/Models/WaitingTime.cs
@@ -0,0 +1,19 @@
+﻿namespace RollercoasterDataAnalytics.Models
+{
+    public class WaitingTime
+    {
+        public DateOnly Date { get; set; }
+        public TimeOnly Time { get; set; }
+        public string? Code { get; set; }
+        public Status Status { get; set; }
+        public required string Name { get; set; }
+    }
+
+    public enum Status
+    {
+        Opened,
+        Closed,
+        Maintenance,
+        ClosedWeather
+    }
+}
--- RollercoasterDataAnalytics/Services/WartezeitenAppService.cs
+++ RollercoasterDataAnalytics/Services/WartezeitenAppService.cs
@@ -1,28 +1,92 @@
 using Microsoft.Extensions.Options;
 using RestSharp;
+using RestSharp.Serializers.Json;
 using RollercoasterDataAnalytics.Configurations;
 using RollercoasterDataAnalytics.Models;
+using System.Text.Json;
 
 namespace RollercoasterDataAnalytics.Services;
 
 public interface IWartezeitenAppService
 {
-    Task<IEnumerable<Park>> GetParks();
+    /// <summary>
+    /// Fetches all parks. The endpoint caches reponses for 24 hours.
+    /// Detailed documentation can be found here: https://api.wartezeiten.app/#/default/get_v1_parks
+    /// </summary>
+    /// <returns></returns>
+    Task<IEnumerable<Park>?> GetParksAsync();
+    /// <summary>
+    /// Fetches all waiting times for the given park. The endpoint caches reponses for 5 minutes.
+    /// Detailed documentation can be found here: https://api.wartezeiten.app/#/default/get_v1_waitingtimes
+    /// </summary>
+    /// <returns></returns>
+    Task<IEnumerable<WaitingTime>?> GetWaitingTimes(Park park) => GetWaitingTimes(park.Id);
+    /// <summary>
+    /// Fetches all waiting times for the given park. The endpoint caches reponses for 5 minutes.
+    /// Detailed documentation can be found here: https://api.wartezeiten.app/#/default/get_v1_waitingtimes
+    /// </summary>
+    /// <returns></returns>
+    Task<IEnumerable<WaitingTime>?> GetWaitingTimes(string parkId);
 }
 
 public class WartezeitenAppService : IWartezeitenAppService
 {
     private readonly ILogger<WartezeitenAppService> _logger;
-    private readonly WartezeitenAppConfiguration _configuration;
     private readonly IRestClient _restClient;
+    private readonly WartezeitenAppConfiguration _configuration;
+    private readonly JsonSerializerOptions _jsonSerializerOptions;
 
     private const string LANGUAGE_HEADER = "language";
+    private const string WAITING_TIMES_QUERY_HEADER = "park";
+    private const string PARKS_ENDPOINT = "parks";
+    private const string WAITING_TIMES_ENDPOINT = "waitingtimes";
 
-    public WartezeitenAppService(ILogger<WartezeitenAppService> logger, IOptions<WartezeitenAppConfiguration> wartezeitenAppConfiguration)
+    public WartezeitenAppService(ILogger<WartezeitenAppService> logger, IOptions<WartezeitenAppConfiguration> wartezeitenAppConfiguration, IOptions<JsonSerializerOptions> jsonSerializerOptions)
     {
         _logger = logger;
         _configuration = wartezeitenAppConfiguration.Value ?? throw new ArgumentNullException($"Failed to load configuration for {typeof(WartezeitenAppConfiguration).Name}");
-        _restClient = new RestClient(_configuration.Uri);
+        _jsonSerializerOptions = jsonSerializerOptions.Value ?? throw new ArgumentNullException($"Failed to load configuration for {typeof(JsonSerializerOptions).Name}");
+        _restClient = new RestClient(_configuration.Uri, configureSerialization: s =>
+        {
+            s.UseSystemTextJson(_jsonSerializerOptions);
+        });
         _restClient.AddDefaultHeader(LANGUAGE_HEADER, _configuration.Language);
-     }
+    }
+
+    public async Task<IEnumerable<Park>?> GetParksAsync()
+    {
+        var request = new RestRequest($"{_configuration.ApiVersion}/{PARKS_ENDPOINT}");
+        try
+        {
+            _logger.LogDebug("Fetching Parks");
+            var response = await _restClient.ExecuteAsync<IEnumerable<Park>>(request).ConfigureAwait(false);
+            if (response.IsSuccessStatusCode)
+                return response.Data;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Failed to fetch parks");
+        }
+
+        return null;
+    }
+
+    public async Task<IEnumerable<WaitingTime>?> GetWaitingTimes(string parkId)
+    {
+        var request = new RestRequest($"{_configuration.ApiVersion}/{WAITING_TIMES_ENDPOINT}");
+        request.AddHeader(WAITING_TIMES_QUERY_HEADER, parkId);
+        try
+        {
+            _logger.LogDebug("Requesting waiting times for park {0}", parkId);
+            var response = await _restClient.ExecuteAsync<IEnumerable<WaitingTime>>(request).ConfigureAwait(false);
+            if (response.IsSuccessStatusCode)
+                return response.Data;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Failed to fetch waiting times for park {0}", parkId);
+        }
+
+        return null;
+    }
 }
